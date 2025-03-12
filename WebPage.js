dfb6c06d9bd4c5f0c10ca58a987edc6294204f21
faxy247 <gem.atkinson@live.co.uk>
2025-03-12 19:13:49 +0000

    Webpage set-up on Localhost

---
+++ WebPage.js
@@ -0,0 +1,54 @@
+const JSONGen = require('./JSON_Generator');
+// Webpage set-up
+const express = require("express");
+const app = express();
+const http = require('http');
+const port = 3000;
+
+
+
+app.use(express.json());
+app.listen(port, () => {
+    console.log("Example app listening at http://***REMOVED***:" + port);
+})
+
+// Used as a general default route to do testing
+app.get("/default_test", async (req,res) => {
+    try {
+        const JSONFile = await JSONGen.getJSONFile(
+            "Walmsley Road",
+            -1.57105,
+            53.81279,
+            "Kingston Terrace",
+            -1.553111,
+            53.8093132
+        );
+
+        res.send(JSONFile)
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// General queries on route
+app.get("/route", async (req,res) => {
+    try {
+        const response = await JSONGen.getJSONFile(
+            req.query.sname,
+            req.query.slat,
+            req.query.slon,
+            req.query.ename,
+            req.query.elat,
+            req.query.elon
+        )
+
+        res.send(response);
+    } catch (err) {
+        let JSON = {};
+        JSON.code = "error";
+        JSON.des = err;
+        console.log("error: " + err);
+        
+        res.send(JSON);
+    }
+})
